2600b6723ca82c7fee91078a36550f39a9b46606
pblankley <31221512+pblankley@users.noreply.github.com>
2018-04-22 22:37:42 -0400

    add rosenbrock example and f(x)=x^2 example

--- src/sampling.py
+++ src/sampling.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-def slice(logp, nsamps, x_init, width=1.0):
+def slice(logp, x_init, width=1.0,max_steps=1000):
     """ Takes in a vector """
-    x_init = np.array(x_init).ravel()
+    x_init = np.array(x_init).astype('float64').ravel()
+    #np.random.shuffle(x_init)
     dim = len(x_init)
     samples,x,xp,L,R = [], x_init.copy(), x_init.copy(), x_init.copy(), x_init.copy()
 
@@ -12,14 +13,23 @@ def slice(logp, nsamps, x_init, width=1.0):
         L[d] = x[d] - width*u
         R[d] = x[d] + width*(1-u)
 
+        #print(yprime,L,R)
         # Step out section 
-        while yprime < logp(L):
+        it=0
+        while yprime < logp(L) and it<max_steps:
             L[d] = L[d]-width
-        while yprime < logp(R):
+            it+=1
+        #print('on to the right',L[d])
+        it = 0
+        while yprime < logp(R) and it<max_steps:
             R[d] = R[d]+width
-        
+            it+=1
+        #print('enter final:',np.random.uniform(L[d],R[d]))
+        #print(type(xp[0]))
         while True:
             xp[d] = np.random.uniform(L[d],R[d])
+            #print(xp[d],np.random.uniform(L[d],R[d]))
+            #print(yprime,logp(xp))
             if yprime < logp(xp):
                 break
             else:
@@ -30,6 +40,7 @@ def slice(logp, nsamps, x_init, width=1.0):
                     L[d] = xp[d]
                 else: 
                     raise ValueError('Step has shrunk to much and cannot continue')
+        #print(xp)
         x[d] = xp[d]
         L[d] = xp[d]
         R[d] = xp[d]
