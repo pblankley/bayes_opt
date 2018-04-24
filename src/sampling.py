19da4ef4ac52c3f9fb679b69923edc7d30faaa90
pblankley <31221512+pblankley@users.noreply.github.com>
2018-04-23 23:33:35 -0400

    add docs to gp and sampling

--- src/sampling.py
+++ src/sampling.py
@@ -1,9 +1,18 @@
 import numpy as np
 
 def slice(logp, x_init, width=1.0,max_steps=1000):
-    """ Takes in a vector """
+    """ Slice sampler for integration over the acquisition functions. This sampler performs one 
+    "slice" and returns the valid sample from the distribution.  The purpose of performing 
+    the one slice is sampling and then computing the improvement metric and then sampling again.
+    -------
+    Args: logp; function, that takes in a vector of shape x_init and returns a probability.
+          x_init; numpy array; should be only 1d
+          width; float, the size of the step when we step out
+          max_steps; int, the maximum number of steps before we quit and just sample 
+    -------
+    Returns: numpy array that is a valid sample from the probability distribution governed by logp
+    """
     x_init = np.array(x_init).astype('float64').ravel()
-    #np.random.shuffle(x_init)
     dim = len(x_init)
     samples,x,xp,L,R = [], x_init.copy(), x_init.copy(), x_init.copy(), x_init.copy()
 
@@ -13,23 +22,18 @@ def slice(logp, x_init, width=1.0,max_steps=1000):
         L[d] = x[d] - width*u
         R[d] = x[d] + width*(1-u)
 
-        #print(yprime,L,R)
         # Step out section 
         it=0
         while yprime < logp(L) and it<max_steps:
             L[d] = L[d]-width
             it+=1
-        #print('on to the right',L[d])
         it = 0
         while yprime < logp(R) and it<max_steps:
             R[d] = R[d]+width
             it+=1
-        #print('enter final:',np.random.uniform(L[d],R[d]))
-        #print(type(xp[0]))
+
         while True:
             xp[d] = np.random.uniform(L[d],R[d])
-            #print(xp[d],np.random.uniform(L[d],R[d]))
-            #print(yprime,logp(xp))
             if yprime < logp(xp):
                 break
             else:
@@ -40,7 +44,7 @@ def slice(logp, x_init, width=1.0,max_steps=1000):
                     L[d] = xp[d]
                 else: 
                     raise ValueError('Step has shrunk to much and cannot continue')
-        #print(xp)
+
         x[d] = xp[d]
         L[d] = xp[d]
         R[d] = xp[d]
